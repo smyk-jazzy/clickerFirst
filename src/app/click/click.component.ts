1cfa703ac738ca162e5d4cd13e4041c456e8c7c1
Smyk <tomasz.jeziorny@wp.pl>
2020-04-04 20:55:25 +0200

    Timer interval intervalOn

--- src/app/click/click.component.ts
+++ src/app/click/click.component.ts
@@ -1,30 +1,31 @@
 import { Component, OnInit, ViewChild, Input } from '@angular/core';
-import {ClickCounterComponent} from '../click-counter/click-counter.component';
+import { ClickCounterComponent } from '../click-counter/click-counter.component';
 
 @Component({
   selector: 'app-click',
   templateUrl: './click.component.html',
-  styleUrls: ['./click.component.css']
+  styleUrls: ['./click.component.css'],
 })
 export class ClickComponent implements OnInit {
-
   clickNumber = 0;
 
-  // @ViewChild('clickCounter')
-  // clickCounterA: ClickCounterComponent;
+  @Input()
+  interval = 2000;
+
+  @Input()
+  intervalOn = false;
 
   @Input()
   clickCounter: ClickCounterComponent;
 
-
   ngOnInit(): void {
+    if (this.intervalOn) {
+      setInterval(() => this.incrementClick(), this.interval);
+    }
   }
 
-  incrementClick(){
+  incrementClick() {
     this.clickNumber += 1;
     this.clickCounter.incrementClick();
   }
-
-
-
 }
